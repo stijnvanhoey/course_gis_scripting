7f6a2e9fd61978a4ea103faf8aa0580cf95767d3
stijnvanhoey <stijnvanhoey@gmail.com>
2017-05-08 18:52:23 +0200

    Update bash handling nbtutor

--- convert_notebooks.sh
+++ convert_notebooks.sh
@@ -1,13 +1,3 @@
-#jupyter nbconvert --to=notebook --config nbconvert_config.py --output "02 - Data structures.ipynb" "solved - 02 - Data structures.ipynb" 
-# jupyter nbconvert --to=notebook --output "03b - Some more advanced indexing.ipynb" "solved - 03b - Some more advanced indexing.ipynb"
-#jupyter nbconvert --to=notebook --config nbconvert_config.py --output "03 - Indexing and selecting data.ipynb" "solved - 03 - Indexing and selecting data.ipynb"
-#jupyter nbconvert --to=notebook --config nbconvert_config.py --output "04 - Groupby operations.ipynb" "solved - 04 - Groupby operations.ipynb"
-
-
-#jupyter nbconvert --to=notebook --config nbconvert_config.py --output "04b - Advanced groupby operations.ipynb" "_solved/"
-#jupyter nbconvert --to=notebook --config nbconvert_config.py --output "05 - Time series data.ipynb" "solved - 05 - Time series data.ipynb"
-#jupyter nbconvert --to=notebook --config nbconvert_config.py --output "06 - Reshaping data.ipynb" "solved - 06 - Reshaping data.ipynb"
-
 
 declare -a arr=(
                 #"00-jupyter_introduction.ipynb"
@@ -21,19 +11,26 @@ declare -a arr=(
                 #"pandas_02_basic_operations.ipynb" 
                 #"pandas_03_selecting_data.ipynb"
                 #"pandas_04_time_series_data.ipynb"
-                "pandas_05_combining_datasets.ipynb"
-                "pandas_06_groupby_operations.ipynb"
-                "pandas_07_reshaping_data.ipynb"
+                #"pandas_05_combining_datasets.ipynb"
+                #"pandas_06_groupby_operations.ipynb"
+                #"pandas_07_reshaping_data.ipynb"
                 #"case1_bike_count.ipynb"
-                "case2_biodiversity_cleaning.ipynb"
-                "case2_biodiversity_analysis.ipynb"
+                #"case2_biodiversity_cleaning.ipynb"
+                #"case2_biodiversity_analysis.ipynb"
+                "00-jupyter_introduction.ipynb"
+                "01-python-introduction.ipynb"
+                "02-scientific-python-introduction.ipynb"
+                "03-geopandas-appetizer.ipynb"
+                "04-gis-python-vectors.ipynb"
+                "05-the-power-of-gdal.ipynb"
+                "06-gis-python-rasters.ipynb"
                 )
 
-
+#jupyter nbconvert 02-scientific-python-introduction.ipynb --to notebook --config ../nbconvert_config.py --output "../notebooks/02-scientific-python-introduction.ipynb"
 for i in "${arr[@]}"
 do
    echo "$i"
    # or do whatever with individual element of the array
    
-   jupyter nbconvert --to=notebook --config ../nbconvert_config.py --output "notebooks/$i" "$i"   
+   jupyter nbconvert "$i" --to notebook --config ../nbconvert_config.py --output "../notebooks/$i"
 done
